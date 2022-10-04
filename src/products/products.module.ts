060b84211ae1926c11ed3e3a627bef61704f917f
jinsung Park <jinsungpark@jinsungui-MacBookPro.local>
2022-10-04 15:52:56 +0900

    충돌 조정

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [ProductsController],
